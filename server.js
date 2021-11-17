212515be47f5efc2acfad91583fb729642281baf
Jared Gavigan <jagavigan80@gmail.com>
2021-11-17 18:42:14 -0500

    final-fix-path

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+
 
 function createNewNote(body, notesArray) {
   const newNote = body;
